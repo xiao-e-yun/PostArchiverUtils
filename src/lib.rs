85f4da1f0139f76983ac17ccc0a31840760f1e18
xiao-e-yun <a0970323059@gmail.com>
2025-09-01 09:28:41 +0800

    feat: add utils

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,16 @@ pub mod display;
 pub mod request;
 pub mod error;
 
+use std::path::{Path, PathBuf};
+use post_archiver::{PostId, POSTS_PRE_CHUNK};
+
 pub use display::*;
 pub use request::*;
 pub use error::*;
+
+pub fn get_post_path(root: impl AsRef<Path>, post: PostId) -> PathBuf {
+    let mut path = root.as_ref().to_path_buf();
+    path.push((*post / POSTS_PRE_CHUNK).to_string());
+    path.push((*post % POSTS_PRE_CHUNK).to_string());
+    path
+}
